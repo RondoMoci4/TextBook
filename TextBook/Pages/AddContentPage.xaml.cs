b504c62e731c3875783e4b0b361221b6bd3b5c35
RondoMovich <rondomovich9@gmail.com>
2022-05-22 22:47:47 +0300

    Добавьте файлы проекта.

---
+++ TextBook/Pages/AddContentPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextBook.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddContentPage.xaml
+    /// </summary>
+    public partial class AddContentPage : Page
+    {
+        public AddContentPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TextBook/Pages/ListThemePage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TextBook.Data;
+using TextBook.Pages;
+
+namespace TextBook.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ListThemePage.xaml
+    /// </summary>
+    public partial class ListThemePage : Page
+    {
+        public ListThemePage()
+        {
+            InitializeComponent();
+            ConnectionClass.connection = new DBTextBookEntities();
+        }
+
+        private void txbSearchTheme_LostFocus(object sender, RoutedEventArgs e) { LostFocusAnimation(txbVisibleSearch); }
+
+        private void txbSearchTheme_GotFocus(object sender, RoutedEventArgs e) { GotFocusAnimation(txbVisibleSearch); }
+
+        private void txbSearchTheme_TextChanged(object sender, TextChangedEventArgs e) { Search(); }
+
+        private void Search()
+        {
+            //ConnectionClass.connection = new DBTextBookEntities();
+            //var themeSearch = ConnectionClass.connection.Test.ToList();
+            //themeSearch = themeSearch.Where(x => x.Title.ToLower().Contains(txbSearchTheme.Text.ToLower())).ToList();
+            //lvTheme.ItemsSource = themeSearch.ToList();
+        }
+
+        private void lvTheme_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btnAddTheme_Click(object sender, RoutedEventArgs e) { FrameClass.mainFrame.Navigate(new AddContentPage()); }
+
+        private void GotFocusAnimation(TextBlock textblock)
+        {
+            TranslateTransform transform = new TranslateTransform();
+            textblock.RenderTransform = transform;
+            DoubleAnimation animationY = new DoubleAnimation(0, -20, TimeSpan.FromSeconds(0.3));
+            transform.BeginAnimation(TranslateTransform.YProperty, animationY);
+            textblock.FontSize = 14;
+        }
+
+        private void LostFocusAnimation(TextBlock textblock)
+        {
+            TranslateTransform transform = new TranslateTransform();
+            textblock.RenderTransform = transform;
+            DoubleAnimation animationY = new DoubleAnimation(-20, 0, TimeSpan.FromSeconds(0.3));
+            transform.BeginAnimation(TranslateTransform.YProperty, animationY);
+            textblock.FontSize = 18;
+        }
+    }
+}
